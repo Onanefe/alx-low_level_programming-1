8721cebed67598bc41d6880b39be5d41d25a9120
Onanefe <106746336+Onanefe@users.noreply.github.com>
2022-08-03 23:45:15 +0100

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,14 +1,37 @@
-#include "function_pointers.h"
-
+#include<stdio.h>
+#include<stdlib.h>
 /**
-  * print_name - Prints a name
-  * @name: The name to prints
-  * @f: Pointer to function
-  *
-  * Return: Nothing
-  */
-void print_name(char *name, void (*f)(char *))
+ * main- Entry point
+(* a blank line
+ *@argc: the number of parameters.
+ *@argv: the parameeters in the case the number ob bytes.
+* Description: this program prints opcodes in hexa)?
+* Return: 0 in succes
+*/
+int main(int argc, char *argv[])
 {
-	if (name != NULL && f != NULL)
-		f(name);
+	int i, n;
+
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i < n - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
 }
